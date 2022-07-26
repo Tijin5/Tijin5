b331f6f4edb01fb9e205a094ccac03a9a0a8d13e
Tijin5 <tijintbabu505@gmail.com>
2022-07-26 12:25:40 +0530

    26 7 22

--- WebProject/views.py
+++ WebProject/views.py
@@ -20,21 +20,14 @@ def profile(request):
 
 def updateProfile(request):
       if request.method=='GET':
-            context = { 'message': 'Success'}
+            context = { 'message': 'Success'}            
+      try:
             formdata = TitleProfile.objects.get(pk=1)
             context['titleform']=TitleProfileForm(instance=formdata)
+      except:
+            context['titleform']=TitleProfileForm()
             print(context)
-            return render(request,'profile-edit.html',context)
-      elif request.method=='POST':
-            print('request.POST',request.POST) 
-            newForm= TitleProfileForm(request.POST)
-            if newForm.is_valid():
-                  context={ 'message':'Success'}
-                  newForm.save()
-            formdata = TitleProfile.objects.get(pk=1)
-            context['titleform']=TitleProfileForm(instance=formdata)
-            print(context)
-            return render(request,'profile-edit.html',context)
+      return render(request,'profile-edit.html',context)
 
 def   contact(request):
       context = {'info':'hello bro'}
