e3488a19246d5c5429a7e604d2a3f36fc19ba54c
Tijin5 <tijintbabu505@gmail.com>
2022-08-16 12:36:25 +0530

    16 aug

--- WebProject/forms.py
+++ WebProject/forms.py
@@ -6,11 +6,12 @@ import datetime
 class InputForm(forms.Form):
   first_name = forms.CharField(max_length = 200)
   last_name = forms.CharField(max_length = 200)
-  # roll_number = forms.IntegerField(help_text = "Enter 6 digit roll")
-  # last_modified = forms.DateTimeField(initial = datetime.datetime.now)
-#   password = forms.CharField(widget = forms.PasswordInput())
+  roll_number = forms.IntegerField(help_text = "Enter 6 digit roll")
+  last_modified = forms.DateTimeField(initial = datetime.datetime.now)
+  password = forms.CharField(widget = forms.PasswordInput())
 class TitleProfileForm(forms.ModelForm):
     class Meta:
         model = TitleProfile
         fields = "__all__"
 
+au
